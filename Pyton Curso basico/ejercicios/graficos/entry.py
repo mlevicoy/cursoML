521acf9003bd7f9fb15a72ac6b4e2402480bb8fd
mlevicoy <mlevicoy@gmail.com>
2022-05-10 10:59:55 -0400

    sdf
    sfsfd

--- Pyton Curso basico/ejercicios/graficos/entry.py
+++ Pyton Curso basico/ejercicios/graficos/entry.py
@@ -6,22 +6,30 @@ miFrame = Frame(raiz, width=1200, height=600)
 miFrame.pack()
 
 cuadroNombre = Entry(miFrame)
-cuadroNombre.grid(row=0, column=1)
+cuadroNombre.grid(row=0, column=1, padx=10, pady=10)
+cuadroNombre.config(fg="red", justify="right")
+
+cuadroPass = Entry(miFrame)
+cuadroPass.grid(row=1, column=1, padx=10, pady=10)
+cuadroPass.config(show="*")
 
 cuadroApellido = Entry(miFrame)
-cuadroApellido.grid(row=1, column=1)
+cuadroApellido.grid(row=2, column=1, padx=10, pady=10)
 
 cuadroDireccion = Entry(miFrame)
-cuadroDireccion.grid(row=2, column=1)
+cuadroDireccion.grid(row=3, column=1, padx=10, pady=10)
 
 nombreLabel = Label(miFrame, text="Nombre:")
-nombreLabel.grid(row=0, column=0)
+nombreLabel.grid(row=0, column=0, sticky="e", padx=10, pady=10)
+
+passLabel = Label(miFrame, text="Password:")
+passLabel.grid(row=1, column=0, sticky="e", padx=10, pady=10)
 
 apellidoLabel = Label(miFrame, text="Apellido:")
-apellidoLabel.grid(row=1, column=0)
+apellidoLabel.grid(row=2, column=0, sticky="e", padx=10, pady=10)
 
 direccionLabel = Label(miFrame, text="Dirección:")
-direccionLabel.grid(row=2, column=0)
+direccionLabel.grid(row=3, column=0, sticky="e", padx=10, pady=10)
 
 
 raiz.mainloop()
