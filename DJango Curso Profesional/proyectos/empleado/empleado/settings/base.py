699e61b3f5805141ad65465b9cf15fbb2bf477f8
mlevicoy <mlevicoy@gmail.com>
2022-05-19 17:17:19 -0400

    19.05.2022
    19.05.2022

--- DJango Curso Profesional/proyectos/empleado/empleado/settings/base.py
+++ DJango Curso Profesional/proyectos/empleado/empleado/settings/base.py
@@ -2,7 +2,8 @@
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+from unipath import Path
+BASE_DIR = Path(__file__).ancestor(3)
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,7 +42,7 @@ ROOT_URLCONF = 'empleado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.child('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
