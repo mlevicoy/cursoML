f2de26d155f31cab31938db855bad0717de236ab
mlevicoy <mlevicoy@gmail.com>
2022-05-23 20:48:26 -0400

    23.05.2022
    23.05.2022

--- DJango Curso Profesional/proyectos/empleado/empleado/applications/home/urls.py
+++ DJango Curso Profesional/proyectos/empleado/empleado/applications/home/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     path('prueba/', views.PruebaView.as_view()),
+    path('lista/', views.PruebaListView.as_view()),
 ]
 
--- DJango Curso Profesional/proyectos/empleado/empleado/applications/home/views.py
+++ DJango Curso Profesional/proyectos/empleado/empleado/applications/home/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 class PruebaView(TemplateView):
-    template_name = 'prueba.html'
+    template_name = 'home/prueba.html'
 
+class PruebaListView(ListView):
+    template_name = "home/lista.html"
+    # Creamos una variable para poder acceder a la lista
+    context_object_name = "listaNumeros"
+    # queryset siempre va hacer referencia a una lista
+    queryset = ['0', '10', '20', '30']
+
+
+    
