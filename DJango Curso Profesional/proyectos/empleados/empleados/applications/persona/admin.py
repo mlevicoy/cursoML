4b203db9a6131b3f7c9506dbbe5f94cc16794e40
mlevicoy <mlevicoy@gmail.com>
2022-07-09 15:16:42 -0400

    09-07-2022
    09-07-2022

--- DJango Curso Profesional/proyectos/empleados/empleados/applications/persona/admin.py
+++ DJango Curso Profesional/proyectos/empleados/empleados/applications/persona/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Empleado
+from .models import Empleado, Habilidades
 
 # Register your models here.
-admin.site.register(Empleado)
+admin.site.register(Habilidades)
+
+# Que los datos de Empleado se vean como una tabla
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = (
+        'firt_name',
+        'last_name',
+        'departamento',
+        'job',
+    )
+    # Filtro buscador (horizontal)
+    search_fields = ('firt_name', )
+    # Filtro vertical
+    list_filter = ('job',)
+    # Fitro horizontal, pasar una opción a otro lado
+    filter_horizontal = ('habilidades',)
+    
+admin.site.register(Empleado, EmpleadoAdmin)
+
+
--- DJango Curso Profesional/proyectos/empleados/empleados/applications/persona/models.py
+++ DJango Curso Profesional/proyectos/empleados/empleados/applications/persona/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from applications.departamento.models import Departamento
 
 # Create your models here.
+class Habilidades(models.Model):
+    habilidad = models.CharField('Habilidad', max_length=50)
+    
+    class meta:
+        verbose_name = 'Habilidad'
+        verbose_name_plural = 'Habilidades Empleados'
+    
+    def __str__(self):
+        return str(self.id) + ' - ' + self.habilidad
+
 class Empleado(models.Model):
     """Modelo para tabla empleado"""
     
@@ -16,11 +26,14 @@ class Empleado(models.Model):
     last_name = models.CharField('Apellidos', max_length=60)
     job = models.CharField('Trabajo', max_length=1, choices=JOB_CHOICES)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='empleado', blank=True, null=True)
+    habilidades = models.ManyToManyField(Habilidades)
     
     class Meta:
         verbose_name = 'Empleado'
         verbose_name_plural = 'Empleados de la empresa'
         ordering = ['last_name']
+        unique_together = ('firt_name', 'departamento')
     
     def __str__(self):
         return str(self.id) + ' - ' + self.firt_name + ' - ' + self.last_name
